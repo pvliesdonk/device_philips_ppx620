11c1e588aafe8f935d3c27b3d7b82e348bb8e807
Peter van Liesdonk <peter@liesdonk.nl>
2020-03-03 17:27:21 +0100

    updated bootimg header version

--- kernel.mk
+++ kernel.mk
@@ -19,6 +19,6 @@ TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 BOARD_KERNEL_CMDLINE := console=ttyFIQ0 androidboot.baseband=N/A androidboot.selinux=permissive androidboot.wificountrycode=XZ androidboot.hardware=rk30board androidboot.console=ttyFIQ0 firmware_class.path=/vendor/etc/firmware init=/init rootwait ro root=PARTUUID=af01642c-9b84-11e8-9b2a-234eb5e198a0 loop.max_part=7
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --second_offset 0x00f00000 --second  $(LOCAL_PATH)/second.img
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --second_offset 0x00f00000 --second  $(LOCAL_PATH)/second.img --header_version 1
 
 TARGET_KERNEL_CONFIG := ppx620_defconfig
